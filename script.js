b8d6453a13b25ae8ebfe74d17cca8079794a87df
chellalilahcen <chellalilahcen@gmail.com>
2025-12-05 12:31:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Navbar color change on scroll
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 50) {
+        document.querySelector('.navbar').classList.add('scrolled');
+    } else {
+        document.querySelector('.navbar').classList.remove('scrolled');
+    }
+});
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
